a8c1110e4e95e8928c614f3c0f2383addd0b3b41
Dave Carter <davemate23@gmail.com>
2015-06-04 21:04:16 +0200

    removed aws gems

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -19,7 +19,6 @@ class EmployeesController < ApplicationController
 
   # GET /employees/new
   def new
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @employee = Employee.new
   end
 
